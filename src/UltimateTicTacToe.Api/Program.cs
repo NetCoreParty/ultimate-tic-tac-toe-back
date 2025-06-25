88704424fc2a6a3c1288f1554a0baf22c6ee5f36
GeorgyLarionov <gvlarionov@alfaleasing.ru>
2025-06-25 05:02:15 +0300

    implemented clean layers approach and cleaned up

--- src/UltimateTicTacToe.Api/Program.cs
+++ src/UltimateTicTacToe.Api/Program.cs
@@ -3,87 +3,94 @@ using UltimateTicTacToe.Storage.Services;
 using UltimateTicTacToe.Storage.HostedServices;
 using UltimateTicTacToe.Core.Services;
 using UltimateTicTacToe.Core.Features.GamePlay;
-using UltimateTicTacToe.Core.Features.RealTimeMoveUpdates;
 using MongoDB.Driver;
 using Microsoft.Extensions.Options;
 using UltimateTicTacToe.Storage.Extensions;
+using UltimateTicTacToe.API.Hubs;
+using UltimateTicTacToe.Core.Features.RealTimeNotification;
+using UltimateTicTacToe.API.RealTimeNotification;
 
-namespace WebApplication1
+namespace UltimateTicTacToe.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        #region Games Repository
+
+        builder.Services.Configure<GameplaySettings>(builder.Configuration.GetSection("GameplaySettings"));
+        builder.Services.AddSingleton<IGameRepository, InMemoryGameRepository>();
+
+        #endregion
+
+        #region Event Store
+
+        builder.Services.Configure<EventStoreSettings>(builder.Configuration.GetSection("EventStoreSettings"));
+        builder.Services.AddGlobalMongoSerialization();
+        builder.Services.AddSingleton<IMongoDatabase>(sp =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var settings = sp.GetRequiredService<IOptions<EventStoreSettings>>().Value;
+            var client = new MongoClient(settings.ConnectionString);
+            return client.GetDatabase(settings.DatabaseName);
+        });
+        builder.Services.AddTransient<IEventStore, MongoEventStore>();
+        builder.Services.AddHostedService<EventStoreInitializer>();
 
-            #region Game Repository
+        #endregion
 
-            builder.Services.Configure<GameplaySettings>(builder.Configuration.GetSection("GameplaySettings"));
-            builder.Services.AddSingleton<IGameRepository, InMemoryGameRepository>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            #endregion
+        builder.Services.AddControllers();
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(MakeMoveCommand).Assembly));
 
-            #region Event Store
+        #region Real Time Notifier
 
-            builder.Services.Configure<EventStoreSettings>(builder.Configuration.GetSection("EventStoreSettings"));
-            builder.Services.AddGlobalMongoSerialization();
-            builder.Services.AddSingleton<IMongoDatabase>(sp =>
-            {
-                var settings = sp.GetRequiredService<IOptions<EventStoreSettings>>().Value;
-                var client = new MongoClient(settings.ConnectionString);
-                return client.GetDatabase(settings.DatabaseName);
-            });
-            builder.Services.AddTransient<IEventStore, MongoEventStore>();
-            builder.Services.AddHostedService<EventStoreInitializer>();
+        builder.Services.AddSignalR();
+        builder.Services.AddTransient<IMoveUpdatesNotificationHub, MoveUpdatesNotificationHub>();
 
-            #endregion
+        #endregion
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        #region CORS
 
-            builder.Services.AddControllers();
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(MakeMoveCommand).Assembly));
-            builder.Services.AddSignalR();
+        builder.Services.Configure<CorsSettings>(builder.Configuration.GetSection("CorsSettings"));
+        var corsConfig = builder.Configuration.GetSection("CorsSettings").Get<CorsSettings>() ?? new CorsSettings();
 
-            #region CORS
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(corsConfig.PolicyName, policy =>
+                policy
+                    .WithOrigins(corsConfig.AllowedOrigins.ToArray())
+                    .WithMethods("GET", "POST", "DELETE")
+                    .WithHeaders("Content-Type", "Authorization", "X-User-Id", "X-Ultimate-TTT-Header")
+                    .AllowCredentials()); // Only if you're using cookies or auth
+        });
 
-            builder.Services.Configure<CorsSettings>(builder.Configuration.GetSection("CorsSettings"));
-            var corsConfig = builder.Configuration.GetSection("CorsSettings").Get<CorsSettings>() ?? new CorsSettings();
+        #endregion
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(corsConfig.PolicyName, policy =>
-                    policy
-                        .WithOrigins(corsConfig.AllowedOrigins.ToArray())
-                        .WithMethods("GET", "POST", "DELETE")
-                        .WithHeaders("Content-Type", "Authorization", "X-User-Id", "X-Ultimate-TTT-Header")
-                        .AllowCredentials()); // Only if you're using cookies or auth
-            });
+        var app = builder.Build();
 
-            #endregion
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseCors(corsConfig.PolicyName);
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            app.UseHttpsRedirection();
-
-            app.MapHub<MoveUpdatesHub>("/move-updates-hub")
-                .RequireCors(corsConfig.PolicyName);
-
-            app.MapControllers();
-
-            app.Run();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseCors(corsConfig.PolicyName);
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+        app.UseHttpsRedirection();
+
+        app.MapHub<MoveUpdatesHub>("/move-updates-hub")
+            .RequireCors(corsConfig.PolicyName);
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
